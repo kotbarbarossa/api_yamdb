eb63dee20597fca46bcd256954230f1ad70aa81d
Kot_Barbarossa <kot_barba@MacBook-Pro-Kot-Barbarossa.local>
2022-08-23 16:20:58 +0300

    Переделана админка под декоратор

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from .models import Category, Genre, Title
 
 
+@admin.register(Title)
 class TitleAdmin(admin.ModelAdmin):
     """Админка произведений."""
     list_display = (
@@ -12,30 +13,25 @@ class TitleAdmin(admin.ModelAdmin):
         'categorie',
         'genre',
     )
-    list_editable = ('categorie',)
     search_fields = ('title',)
     list_filter = ('year',)
-    empty_value_display = '-пусто-'
 
 
-class CategoriesAdmin(admin.ModelAdmin):
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Админка категроий."""
     list_display = (
         'title',
         'slug',
     )
     search_fields = ('title',)
-    empty_value_display = '-пусто-'
 
 
+@admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
+    """Админка жанров."""
     list_display = (
         'title',
         'slug',
     )
     search_fields = ('title',)
-    empty_value_display = '-пусто-'
-
-
-admin.site.register(Title, TitleAdmin)
-admin.site.register(Category, CategoriesAdmin)
-admin.site.register(Genre, GenreAdmin)
