4f5e5d9c6b17f29fa30524416ede884989978c6c
Marsel <marsel168@bk.ru>
2022-08-23 19:16:11 +0300

    Создана модель User. Добавлена админка UserAdmin

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'role',
+    )
+    fieldsets = (
+        (None, {
+            'fields': (
+                'username',
+                'email',
+                'first_name',
+                'last_name',
+                'role',
+                'bio',
+            )
+        }),
+    )
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+ROLE = (
+    (
+        ('user', 'Пользователь'),
+        ('admin', 'Администратор'),
+        ('moderator', 'Модератор'),
+    )
+)
+
+
+class User(AbstractUser):
+    """Модель пользователя."""
+    role = models.CharField(
+        'Пользовательская роль',
+        max_length=10,
+        choices=ROLE,
+        default=''
+    )
+    bio = models.TextField(
+        'Биография',
+        blank=True
+    )
