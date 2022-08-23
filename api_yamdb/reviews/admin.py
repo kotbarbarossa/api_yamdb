ef31b90e4d202e25df8453b3859d32a41aa6def4
Kot_Barbarossa <kot_barba@MacBook-Pro-Kot-Barbarossa.local>
2022-08-23 15:44:23 +0300

    Созданы модели. Добавлена админка

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Genre, Title
+
+
+class TitleAdmin(admin.ModelAdmin):
+    """Админка произведений."""
+    list_display = (
+        'title',
+        'description',
+        'year',
+        'categorie',
+        'genre',
+    )
+    list_editable = ('categorie',)
+    search_fields = ('title',)
+    list_filter = ('year',)
+    empty_value_display = '-пусто-'
+
+
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'slug',
+    )
+    search_fields = ('title',)
+    empty_value_display = '-пусто-'
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'slug',
+    )
+    search_fields = ('title',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Title, TitleAdmin)
+admin.site.register(Category, CategoriesAdmin)
+admin.site.register(Genre, GenreAdmin)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
-# Create your models here.
+
+class Category:
+    """Модель для категорий. Присваевается одна на произведение"""
+    title = models.CharField(max_length=48, verbose_name='Название категории')
+    slug = models.SlugField(max_length=48, unique=True)
+
+    def __str__(self):
+        return self.slug
+
+
+class Genre:
+    """Модель для Жанров. Множественное пристваивание на произведение"""
+    title = models.CharField(max_length=48, verbose_name='Название жанра')
+    slug = models.SlugField(max_length=48, unique=True)
+
+    def __str__(self):
+        return self.slug
+
+
+class Title(models.Model):
+    """Модель художественного произведения"""
+    title = models.CharField(
+        max_length=128,
+        verbose_name='Название произведения'
+    )
+    description = models.TextField(
+        max_length=256,
+        blank=True,
+        null=True,
+        verbose_name='Описание')
+    year = models.IntegerField(verbose_name='Год выхода')
+    categorie = models.ForeignKey(
+        Category,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        verbose_name='Категория'
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        through='TitleGenre',
+        related_name='titles',
+        verbose_name='Жанр'
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class TitleGenre(models.Model):
+    """Модель связывающая произведение с жанрами"""
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title} относится к жанру {self.genre}'
