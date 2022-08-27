c98c7c1d13561fc2027aafbe803dc27f151b2659
Kot_Barbarossa <kot_barba@MacBook-Pro-Kot-Barbarossa.local>
2022-08-27 13:41:04 +0300

    Merge commit.

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, filters
 from django.db.models import Avg
 from django.shortcuts import get_object_or_404
 from reviews.models import Category, Genre, Title, Review
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .serializers import (
     CategorySerializer,
@@ -26,22 +27,34 @@ class CategoryGenreViewSet(
 class CategoryViewSet(CategoryGenreViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter,)
+    search_fields = ('name',)
 
 
 class GenreViewSet(CategoryGenreViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter,)
+    search_fields = ('name',)
 
 
 class TitleViewSet(viewsets.ModelViewSet):
     """Класс для модели Title"""
     review = Review.objects.all()
     queryset = Title.objects.annotate(rating=Avg('reviews__score'))
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter,)
+    # search_fields = ('following__username',)
+    search_fields = (
+        'name',
+        'year',
+        'category__slug',
+        # 'genre_slug',
+    )
     # permission_classes = ()
     # pagination_class = LimitOffsetPagination
 
     def get_serializer_class(self):
-        if self.request.method in ('POST', 'PATCH',):
+        if self.request.method in ('POST', 'PATCH', 'PUT',):
             return TitleWriteSerializer
         return TitleSerializer
 
