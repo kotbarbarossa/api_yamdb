fc0508924b8355eff5a3788288757028f70c18ab
Николай Слесарев <kolansers@yandex.ru>
2022-08-31 11:13:09 +0300

    Исправлены замечания по urls, создана папка v1, в которой реализован API v1, добавлен auth_patterns

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,35 +1,8 @@
 from django.urls import include, path
-from rest_framework import routers
-from api.views import (
-    CategoryViewSet,
-    GenreViewSet,
-    TitleViewSet,
-    CommentViewSet,
-    ReviewViewSet,
-    SignUpView,
-    UserViewSet,
-    MyTokenObtainPairView
-)
 
-v1_router = routers.DefaultRouter()
-v1_router.register(r'users', UserViewSet, basename='users')
-v1_router.register(r'categories', CategoryViewSet, basename='сategories')
-v1_router.register(r'genres', GenreViewSet, basename='genres')
-v1_router.register(r'titles', TitleViewSet, basename='titles')
-v1_router.register(
-    r'titles/(?P<title_id>\d+)/reviews',
-    ReviewViewSet,
-    basename='review'
-)
-v1_router.register(
-    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
-    CommentViewSet,
-    basename='comment'
-)
+from .v1.urls import urlpatterns as urlpatterns_v1
+
 
 urlpatterns = [
-    path('v1/', include(v1_router.urls)),
-    path('v1/auth/signup/', SignUpView.as_view(), name='signup'),
-    path('v1/auth/token/', MyTokenObtainPairView.as_view(),
-         name='token_obtain_pair'),
+    path('v1/', include(urlpatterns_v1)),
 ]
